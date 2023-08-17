09aebea921aa2729644a427fce0b737c64e48f23
jam6123 <139221130+jam6123@users.noreply.github.com>
2023-08-17 12:44:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 const express = require('express')
 const app = express()
-// const mysql = require('mysql')
-
-// const connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'rootadmin',
-//   password : 'fiejdl3#jfwjX',
-//   database : 'id20081623_firstdb',
-//   port: 3306
-// });
- 
-// const SQL_STATEMENT = `INSERT INTO users (username, password) VALUES("?", "?")`
-
-// app.use(express.json())
-
-// connection.end();
-// app.post('/', function(req, res) {
-//     console.log(req.body)
-//     connection.connect();
-     
-//     connection.query(SQL_STATEMENT, [req.body.username, req.body.password], function (error, results, fields) {
-//       if (error) throw error;
-//       console.log(results);
-//     });
-     
-//     res.end()
-// })
 
 const users = [
     {id: 1, username: 'yassi'},
